9b8055be51aa43bab66794874a9f0322ccd961d0
XLearner <yan8277@qq.com>
2017-07-16 08:57:30 +0800

    Add files via upload

---
+++ player/js/lanrenzhijia.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+  var playlist = [{
+      title:"Hidden",
+      artist:"(Punch)",
+      mp3:"images/StayWithMe.mp3",
+      poster: "images/timg.jpg"
+    }];
+  
+  var cssSelector = {
+    jPlayer: "#jquery_jplayer",
+    cssSelectorAncestor: ".music-player"
+  };
+  
+  var options = {
+    swfPath: "http://cdnjs.cloudflare.com/ajax/libs/jplayer/2.6.4/jquery.jplayer/Jplayer.swf",
+    supplied: "ogv, m4v, oga, mp3"
+  };
+  
+  var myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
+  
+});
